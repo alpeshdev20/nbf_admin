e6c95afcd7313fa49b15a1274b54024e5b87662a
vishvasartechnolabs <vishvas@artechnolabs.com>
2024-09-09 13:11:38 +0530

    Fix teacher creation issue and update email template with dynamic content

---
+++ resources/views/emails/teacher_subscription.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Your NetbookFlix Account</title>
+</head>
+<body>
+    <h1>Hello {{ $teacherName }},</h1>
+    <p>Your account has been created successfully with the following details:</p>
+    <p><strong>Password:</strong> {{ $password }}</p>
+    <p>Regards,</p>
+    <p>Netbookflix</p>
+</body>
+</html>
